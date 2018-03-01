cbe665b3c7337f6af7f04464fdd505fec20e9865
Sia <siavash.malekpour@gmail.com>
2018-03-01 16:06:40 -0500

    Update version
    https://trello.com/c/6KWY9NZj

--- src/examples.js
+++ src/examples.js
@@ -1,7 +1,8 @@
 import Gapi from './gapi-js';
 
 
-const g = new Gapi({ key: 'xxx', proxy: 'version=alldossiers' });
+const p = new Gapi({ key: 'live_791be390acce8bea78aa0c7795bda295c12281a8'});
+// const p = new GapiPrivate({ key: 'live_791be390acce8bea78aa0c7795bda295c12281a8'});
 
 
 const query = `query query {
@@ -26,6 +27,16 @@ const variables = {
 //   console.log(res.body.data)
 // });
 
-g.itineraries.get(2216, 2555).end( (err, res) => {
-  console.log(res)
+// g.itineraries.get(2216, 2555).end( (err, res) => {
+//   console.log(res)
+// });
+
+p.activity_dossiers.get(6965).end( (err, res) => {
+  console.log(res.body)
+  console.log('-------------------')
+});
+
+p.reporting_offices.get(14).end( (err, res) => {
+  console.log(res.body)
+  console.log('=============')
 });
